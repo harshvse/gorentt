aab7ed82012d052cd94f5fd328d7e932f9d9c7cd
Harsh Verma <harsh.v@jmangroup.com>
2025-03-14 13:21:07 +0000

    added debian torrent and able to parse things like announce and name

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
-	"strings"
+	"io"
+	"os"
 
 	"github.com/jackpal/bencode-go"
 )
 
-type Torrent struct {
+type BenTorrent struct {
 	Announce     string      `bencode:"announce"`
 	Comment      string      `bencode:"comment"`
 	CreationDate int         `bencode:"creation date"`
@@ -21,15 +23,89 @@ type TorrentInfo struct {
 	Name        string `bencode:"name"`
 }
 
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	Pieces      [][20]byte
+	PieceLength int
+	Length      int
+	name        string
+}
+
+func (i *TorrentInfo) splitPieceHashes() ([][20]byte, error) {
+	hashLen := 20
+	buf := []byte(i.Pieces)
+	if len(buf)%hashLen != 0 {
+		err := fmt.Errorf("What in the world are these pieces harsh! %d", len(buf))
+		return nil, err
+	}
+	numHashes := len(buf) / hashLen
+	hashes := make([][20]byte, numHashes)
+
+	for i := 0; i < numHashes; i++ {
+		copy(hashes[i][:], buf[i*hashLen:(i+1)*hashLen])
+	}
+	return hashes, nil
+}
+
+func (benTorrent BenTorrent) generateInfoHash() [20]byte {
+
+	infoHash := sha1.New()
+	infoHash.Write([]byte(benTorrent.Info.Name))
+	infoHash.Write([]byte(benTorrent.Info.Pieces))
+	infoHash.Write([]byte(benTorrent.Info.Pieces))
+	infoHash.Write([]byte(string(benTorrent.Info.PieceLength)))
+
+	return [20]byte(infoHash.Sum(nil))
+}
+
+func (benTorrent BenTorrent) ToTorrentFile() (*TorrentFile, error) {
+	torrentFile := TorrentFile{}
+
+	torrentFile.Announce = benTorrent.Announce
+
+	torrentFile.name = benTorrent.Info.Name
+
+	torrentFile.InfoHash = benTorrent.generateInfoHash()
+
+	torrentFile.PieceLength = benTorrent.Info.PieceLength
+
+	pieces, err := benTorrent.Info.splitPieceHashes()
+	torrentFile.Pieces = pieces
+	if err != nil {
+		return nil, err
+	}
+	return &torrentFile, nil
+}
+func ReadTorrent(r io.Reader) (*BenTorrent, error) {
+	benTorrentFile := BenTorrent{}
+	if err := bencode.Unmarshal(r, &benTorrentFile); err != nil {
+		return nil, err
+	}
+	return &benTorrentFile, nil
+}
+
 func main() {
 
-	sampleTorrent := `d8:announce41:http://bttracker.debian.org:6969/announce7:comment35:"Debian CD from cdimage.debian.org"13:creation datei1573903810e9:httpseedsl145:https://cdimage.debian.org/cdimage/release/10.2.0//srv/cdbuilder.debian.org/dst/deb-cd/weekly-builds/amd64/iso-cd/debian-10.2.0-amd64-netinst.iso145:https://cdimage.debian.org/cdimage/archive/10.2.0//srv/cdbuilder.debian.org/dst/deb-cd/weekly-builds/amd64/iso-cd/debian-10.2.0-amd64-netinst.isoe4:infod6:lengthi351272960e4:name31:debian-10.2.0-amd64-netinst.iso12:piece lengthi262144e6:pieces26800:�����PS�^�� (binary blob of the hashes of each piece)ee`
+	file, err := os.Open("debian.torrent")
+	if err != nil {
+		fmt.Printf("Failed to open torrent file. Error: %v\n", err)
+		return
+	}
+	defer file.Close()
+	benTorrentFile, err := ReadTorrent(file)
 
-	r := strings.NewReader(sampleTorrent)
-
-	torrentFile := Torrent{}
-	if err := bencode.Unmarshal(r, &torrentFile); err != nil {
-		fmt.Errorf("Failed to read torrent file")
+	if err != nil {
+		fmt.Printf("Failed to read torrent file. Error: %s", err.Error())
+		return
 	}
 
+	torrentFile, err := benTorrentFile.ToTorrentFile()
+	if err != nil {
+		fmt.Printf("Failed to convert into torrent file. Error: %s", err)
+		return
+	}
+
+	fmt.Println(torrentFile.Announce)
+	return
 }
