287cdc3ac6a6f4eb06b0b1960d6590dc6ad71203
Harsh Verma <harsh.v@jmangroup.com>
2025-03-28 12:58:46 +0000

    added comments for better visibility

--- src/main.go
+++ src/main.go
@@ -27,12 +27,14 @@ func main() {
 		return
 	}
 
+	// convert the bentorrent file to torrent file which includes info_hash
 	torrentFile, err := benTorrentFile.ToTorrentFile()
 	if err != nil {
 		fmt.Printf("Failed to convert into torrent file. Error: %s", err)
 		return
 	}
 
+	// TODO create a random torrent client ID for each user
 	clientID := []byte("gorentt-k8hj0wgej6ch")
 	url, err := torrentFile.buildTrackerURL([20]byte(clientID), 9888)
 
@@ -41,6 +43,7 @@ func main() {
 		return
 	}
 
+	// send GET request to the announce URL which returns Interval and Binary of peer ip and port
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -48,18 +51,21 @@ func main() {
 	}
 	defer resp.Body.Close()
 
+	// convert the contents of body to []byte
 	bencodeResponse, err := io.ReadAll(resp.Body)
-
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+
+	// unmarshal the Interval and peers from bencode to a struct
 	peersBin := PeerBin{}
 	bencodeResponseReader := bytes.NewReader(bencodeResponse)
 	if err := bencode.Unmarshal(bencodeResponseReader, &peersBin); err != nil {
 		fmt.Println("failed to read peers bin from response")
 	}
 
+	// read the IP and Port for each peer and convert it to a []Peers
 	peer, err := UnmarshalPeers([]byte(peersBin.Peers))
 
 	if err != nil {
